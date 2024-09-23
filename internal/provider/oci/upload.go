f62bb3a8094b58fc0fedc8086e8578ecd98a8937
Pramod Bindal <prbindal@redhat.com>
2024-09-23 16:57:26 +0530

    SRVKP-6442 - use a common  environment varibale cred-store for all types of providers.

--- internal/provider/oci/upload.go
+++ internal/provider/oci/upload.go
@@ -16,6 +16,11 @@ import (
 )
 
 func Upload(_ context.Context, hash, target, folder string, insecure bool) error {
+	credStore := os.Getenv("CRED_STORE")
+	if credStore != "" {
+		os.Setenv("DOCKER_CONFIG", os.Getenv("CRED_STORE"))
+	}
+	fmt.Println("DOCKER_CONFIG : ", os.Getenv("DOCKER_CONFIG"))
 	cacheImageRef := strings.ReplaceAll(target, "{{hash}}", hash)
 	fmt.Fprintf(os.Stderr, "Upload %s content to oci image %s\n", folder, cacheImageRef)
 
