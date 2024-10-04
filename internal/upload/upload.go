c85403af8090bd00662bd0f90c8a626433f77590
Khurram Baig <kbaig@redhat.com>
2024-10-05 02:49:55 +0530

    Add Blob pattern for GCS and AWS

--- internal/upload/upload.go
+++ internal/upload/upload.go
@@ -3,41 +3,25 @@ package upload
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/url"
-	"os"
 	"strings"
 
-	"github.com/openshift-pipelines/tekton-caches/internal/tar"
-
-	"github.com/openshift-pipelines/tekton-caches/internal/provider/s3"
-
-	"github.com/openshift-pipelines/tekton-caches/internal/provider/gcs"
+	"github.com/openshift-pipelines/tekton-caches/internal/provider/blob"
 	"github.com/openshift-pipelines/tekton-caches/internal/provider/oci"
 )
 
 func Upload(ctx context.Context, hash, target, folder string, insecure bool) error {
+	target = strings.ReplaceAll(target, "{{hash}}", hash)
 	u, err := url.Parse(target)
 	if err != nil {
 		return err
 	}
 	newTarget := strings.TrimPrefix(target, u.Scheme+"://")
-	newTarget = strings.ReplaceAll(newTarget, "{{hash}}", hash)
-	tarFile, err := os.CreateTemp("", "cache.tar")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := tar.Tarit(folder, tarFile.Name()); err != nil {
-		return err
-	}
-	defer os.Remove(tarFile.Name())
 	switch u.Scheme {
 	case "oci":
 		return oci.Upload(ctx, hash, newTarget, folder, insecure)
-	case "s3":
-		return s3.Upload(ctx, newTarget, tarFile.Name())
-	case "gs":
-		return gcs.Upload(ctx, hash, newTarget, folder)
+	case "s3", "gs":
+		return blob.Upload(ctx, *u, folder)
 	default:
 		return fmt.Errorf("unknown schema: %s", target)
 	}
