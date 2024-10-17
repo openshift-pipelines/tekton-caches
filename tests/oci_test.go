69561ae495a9de3bc7ccc7f2c5e93cddf6fff628
PuneetPunamiya <ppunamiy@redhat.com>
2024-10-17 14:38:08 +0530

    Adds e2e tests for oci tekton cache
    Signed-off-by: PuneetPunamiya <ppunamiy@redhat.com>

--- tests/oci_test.go
+++ tests/oci_test.go
@@ -12,9 +12,14 @@ import (
 	"github.com/openshift-pipelines/tekton-caches/internal/fetch"
 	"github.com/openshift-pipelines/tekton-caches/internal/hash"
 	"github.com/openshift-pipelines/tekton-caches/internal/upload"
+	"github.com/openshift-pipelines/tekton-caches/tests/client"
+	"github.com/openshift-pipelines/tekton-caches/tests/resources"
+	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"gotest.tools/v3/assert"
 	"gotest.tools/v3/env"
 	"gotest.tools/v3/fs"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
 )
 
 const (
@@ -40,4 +45,69 @@ func TestOCIUpload(t *testing.T) {
 	assert.NilError(t, upload.Upload(ctx, hash, regTarget, tmpdir.Path(), true))
 	assert.NilError(t, fetch.Fetch(ctx, hash, regTarget, tmpdir.Path(), false))
 	assert.NilError(t, fetch.Fetch(ctx, "unknown", regTarget, tmpdir.Path(), false)) // should not error on unknown hash
+}
+
+func TestCacheOCI(t *testing.T) {
+	ctx := context.Background()
+	p := new(tektonv1.Pipeline)
+	pr := new(tektonv1.PipelineRun)
+
+	// Get the pipeline yaml
+	pipeline, err := os.ReadFile("test-pipelineruns/test-pipeline-oci.yaml")
+	if err != nil {
+		t.Fatalf("Error reading file: %v", err)
+	}
+	if err := yaml.UnmarshalStrict(pipeline, p); err != nil {
+		t.Fatalf("Error unmarshalling: %v", err)
+	}
+
+	// Get the pipelineRun yaml
+	pipelineRun, err := os.ReadFile("test-pipelineruns/test-pipelinerun-oci.yaml")
+	if err != nil {
+		t.Fatalf("Error reading file: %v", err)
+	}
+	if err := yaml.UnmarshalStrict(pipelineRun, pr); err != nil {
+		t.Fatalf("Error unmarshalling: %v", err)
+	}
+
+	tc := client.TektonClient(t)
+
+	// Install the pipeline example
+	if _, err := tc.Pipelines(resources.DefaultNamespace).Create(ctx, p, metav1.CreateOptions{}); err != nil {
+		t.Fatalf("Error creating Pipeline: %v", err)
+	}
+
+	_ = tc.PipelineRuns(resources.DefaultNamespace).Delete(ctx, pr.GetName(), metav1.DeleteOptions{
+		PropagationPolicy: &resources.DeletePolicy,
+	})
+
+	// Install the pipelineRun example
+	if _, err = tc.PipelineRuns(resources.DefaultNamespace).Create(ctx, pr, metav1.CreateOptions{}); err != nil {
+		t.Fatalf("Error creating PipelineRun: %v", err)
+	}
+
+	if err := resources.WaitForPipelineRun(ctx, tc, pr, resources.Succeed(pr.Name), resources.WaitInterval); err != nil {
+		t.Fatalf("Error waiting for PipelineRun to complete: %v", err)
+	}
+
+	// Get the taskrun
+	tr, err := tc.TaskRuns(resources.DefaultNamespace).Get(ctx, "pipelinerun-oci-test-build-task", metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("Error creating PipelineRun: %v", err)
+	}
+
+	// Assert the result which was produced by stepAction and stored as result in taskrun
+	assert.Equal(t, tr.Status.Results[0].Value.StringVal, "true")
+
+	// Delete the pipelinerun
+	err = tc.PipelineRuns(resources.DefaultNamespace).Delete(ctx, pr.GetName(), metav1.DeleteOptions{})
+	if err != nil {
+		t.Fatalf("Error deleting pipelinerun")
+	}
+
+	// Delete the pipeline
+	err = tc.Pipelines(resources.DefaultNamespace).Delete(ctx, p.GetName(), metav1.DeleteOptions{})
+	if err != nil {
+		t.Fatalf("Error deleting pipelinerun")
+	}
 }
